2effd5a98c56f1e46d19fe87130db55429ed1ac8
Nayan Dey <ndeynayan123@gmail.com>
2026-03-07 02:13:11 +0530

    add active style in filter buttons

--- script/script.js
+++ script/script.js
@@ -1,1 +1,24 @@
 // console.log("hello")
+
+// Feature 1: toggle b/w filter buttons
+let filterButtons = document.querySelectorAll("#filter-button .btn");
+console.log(filterButtons);
+
+
+// remove active from all button
+const remoteActive = () => {
+    filterButtons.forEach(btn => {
+        btn.classList.remove("active");
+    })
+}
+
+// active button 
+filterButtons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        // console.log(btn);
+        remoteActive();
+        btn.classList.add("active");
+
+    })
+})
+
